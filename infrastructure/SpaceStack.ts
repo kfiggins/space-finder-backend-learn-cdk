c661458f4bb1a313fc427db112825e0e148eecac
Kyler Figgins <kyler.figgins@gmail.com>
2022-12-09 09:11:15 -0500

    Added first lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,7 +1,15 @@
 import * as cdk from "aws-cdk-lib";
+import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
 
 export class SpaceStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
     super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
   }
 }
